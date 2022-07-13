d5e6dfc65dacfe1fd5b233d9660c33fb21389c28
Nir Razo <nir@wiply.com>
2022-07-13 15:04:50 +0300

    link

--- script.js
+++ script.js
@@ -55,13 +55,13 @@ function flip(flipCard, newNumber) {
 function addDynamicLink() {
   const queryParams = new URLSearchParams(window.location.search);
   const link = queryParams.get('link');
-
-  const baseurl = "https://mega1.wiply-dev.xyz/?link=" + link;
+  const name = queryParams.get('name');
+  document.getElementsByClassName('name')[0].innerText = name;
+  const random = queryParams.get('random');
+  const baseurl = "https://mega1.wiply-dev.xyz/?link=" + link +"&random="+random+"&name="+name ;
       const res = baseurl.replaceAll(" ","%20")
-      document.getElementById('smallLink').innerText = res;
+      document.getElementById('smallLink').innerText = res ;
       document.getElementsByClassName('facebook')[0].href = "https://www.facebook.com/sharer/sharer.php?u=" +res
       document.getElementsByClassName('whatapp')[0].href = "https://api.whatsapp.com/send?text= "+ res; 
-      const name = queryParams.get('name');
-      document.getElementsByClassName('name')[0].innerText = name;
 }
 addDynamicLink()
