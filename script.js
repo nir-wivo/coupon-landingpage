edfb50da1e354e202ff865f827854699da6d9d59
Nir Razo <nir@wiply.com>
2022-07-13 15:44:44 +0300

    whatapp

--- script.js
+++ script.js
@@ -59,9 +59,9 @@ function addDynamicLink() {
   document.getElementsByClassName('name')[0].innerText = name;
   const random = queryParams.get('random');
   const baseurl = "https://mega1.wiply-dev.xyz/?link=" + link +"&"+random+"&name="+name ;
-      const res = baseurl.replaceAll(" ","%20")
-      document.getElementById('smallLink').innerText = res ;
+      const res = baseurl.replaceAll(" ","%20");
+      document.getElementById('smallLink').innerText = res; 
       document.getElementsByClassName('facebook')[0].href = "https://www.facebook.com/sharer/sharer.php?u=" +res;
-      document.getElementsByClassName('whatapp')[0].href = "https://api.whatsapp.com/send?text= "+ res; 
+      document.getElementsByClassName('whatapp')[0].href = "https://api.whatsapp.com/send?text="+ res.replaceAll("&","%26");
 }
 addDynamicLink()
