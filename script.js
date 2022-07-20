a5da994cea6fdd7ce355400ce8a20d5e9273914e
user name <user email>
2022-07-20 12:02:08 +0300

    אקדא

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const countToDate = new Date("2022-08-01T00:00:00");
+const countToDate = new Date("2022-08-20T00:00:00");
 let previousTimeBetweenDates
 setInterval(() => {
   const currentDate = new Date()
