d2daa31c203dd17ea3cec86657de62170428541e
user name <user email>
2022-10-19 17:14:54 +0300

    gift

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function addDynamicLink() {
     const queryParams = new URLSearchParams(window.location.search);
-    const score = queryParams.get("score");
-    document.getElementsByClassName("score")[0].innerText = score;
     const name = queryParams.get("name");
     document.getElementsByClassName("name")[0].innerText = name;
   }
